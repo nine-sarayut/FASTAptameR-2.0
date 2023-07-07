044ea268a623c7cdeb6cdfa566dec9d82fa5b659
Sarayut Winuthayanon <s.winuthayanon@missouri.edu>
2023-07-07 10:57:13 -0500

    Updated

--- Nine.txt.sh
+++ Nine.txt.sh
@@ -1,9 +1,11 @@
-docker build -t winuthayanon/fastaptamer-2.0:20230706 . -f Dockerfile
-docker push winuthayanon/fastaptamer-2.0:20230706
 docker build -t winuthayanon/fastaptamer-2.0:4.3.0 . -f Dockerfile
 docker push winuthayanon/fastaptamer-2.0:4.3.0
 docker build -t winuthayanon/fastaptamer-2.0:4.3.0-1 . -f Dockerfile
 docker push winuthayanon/fastaptamer-2.0:4.3.0-1
+docker build -t winuthayanon/fastaptamer-2.0:4.3.1 . -f Dockerfile
+docker push winuthayanon/fastaptamer-2.0:4.3.1
+docker build -t winuthayanon/fastaptamer-2.0:4.3.1-1 . -f Dockerfile
+docker push winuthayanon/fastaptamer-2.0:4.3.1-1
 
 # docker run -v "$(pwd)/nine:/app/user_work" -v /var/run/docker.sock:/var/run/docker.sock \
 # -p 4949:4949 \
@@ -12,16 +14,31 @@ docker push winuthayanon/fastaptamer-2.0:4.3.0-1
 
 docker run \
 -p 4949:4949 \
--it winuthayanon/fastaptamer-2.0:20230706 \
+-it winuthayanon/fastaptamer-2.0:4.3.0 \
 R -e "shiny::runApp('/srv/shiny-server', host = '0.0.0.0', port = 4949, launch.browser = FALSE)"
 
-rm -f fastaptamer-2.0_20230706.sif
-singularity pull docker://winuthayanon/fastaptamer-2.0:20230706
-singularity inspect fastaptamer-2.0_20230706.sif
+docker run \
+-p 4949:4949 \
+-it winuthayanon/fastaptamer-2.0:4.3.0-1 \
+R -e "shiny::runApp('/srv/shiny-server', host = '0.0.0.0', port = 4949, launch.browser = FALSE)"
+
+docker run \
+-p 4949:4949 \
+-it winuthayanon/fastaptamer-2.0:4.3.1 \
+R -e "shiny::runApp('/srv/shiny-server', host = '0.0.0.0', port = 4949, launch.browser = FALSE)"
+
+docker run \
+-p 4949:4949 \
+-it winuthayanon/fastaptamer-2.0:4.3.1-1 \
+R -e "shiny::runApp('/srv/shiny-server', host = '0.0.0.0', port = 4949, launch.browser = FALSE)"
+
+rm -f fastaptamer-2.0_4.3.1-1.sif
+singularity pull docker://winuthayanon/fastaptamer-2.0:4.3.1-1
+singularity inspect fastaptamer-2.0_4.3.1-1.sif
 
 readonly PORT=$(python -c 'import socket; s=socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 echo $PORT
-SIF_FILE=fastaptamer-2.0_20230706.sif
+SIF_FILE=fastaptamer-2.0_4.3.1-1.sif
 singularity exec \
   --writable-tmpfs \
   --cleanenv ${SIF_FILE} \
